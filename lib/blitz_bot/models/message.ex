51003f37913245e16bb3fa294c791339a02aa7d5
Artem <q3pp@yandex.ru>
2019-11-16 10:10:15 +0100

    fix associations

--- lib/blitz_bot/models/message.ex
+++ lib/blitz_bot/models/message.ex
@@ -3,7 +3,7 @@ defmodule BlitzBot.Message do
   alias BlitzBot
 
   schema "message" do
-    belongs_to :user, User
-    belongs_to :chat, Chat
+    belongs_to :user, BlitzBot.User
+    belongs_to :chat, BlitzBot.Chat
   end
 end
